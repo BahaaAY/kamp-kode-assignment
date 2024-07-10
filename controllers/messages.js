72504d3416f601fcccd8eee0a45eb8321b4a6ec2
BahaaAY <bahaa.abouyassine2@gmail.com>
2024-07-10 08:41:22 +0300

    add validation on routes ✅

--- controllers/messages.js
+++ controllers/messages.js
@@ -1,12 +1,25 @@
+const { validationResult } = require("express-validator");
 const Message = require("../models/message");
 exports.addMessage = async (req, res, next) => {
   try {
+    const validationErrors = validationResult(req);
+    if (!validationErrors.isEmpty()) {
+      const error = new Error("Validation failed");
+      error.statusCode = 400;
+      error.data = validationErrors.array();
+      throw error;
+    }
     const { name, email, message } = req.body;
     console.log(name, email, message);
     const newMessage = new Message(name, email, message);
     await newMessage.save();
-    res.redirect("/message");
+    res.render("index", { messageSent: true, errors: null });
   } catch (err) {
     console.log(err);
+    res.statusCode = err.statusCode || 500;
+    res.render("index", {
+      messageSent: false,
+      errors: err.data || ["Internal server error"],
+    });
   }
 };
--- routes/messages.js
+++ routes/messages.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const router = express.Router();
-
+const { body } = require("express-validator");
 const { addMessage } = require("../controllers/messages");
 
 router.get("/message", (req, res) => {
-  res.render("index");
+  res.render("index", { messageSent: false, errors: null });
 });
 
-router.post("/message", addMessage);
+router.post(
+  "/message",
+  [
+    body("name").trim().notEmpty().withMessage("Name cannot be empty!"),
+    body("email")
+      .trim()
+      .notEmpty()
+      .withMessage("Email cannot be empty!")
+      .isEmail()
+      .withMessage("Invalid email!"),
+    body("message").trim().notEmpty().withMessage("Message cannot be empty!"),
+  ],
+  addMessage
+);
 
 module.exports = router;
